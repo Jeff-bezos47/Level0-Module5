d310ae6b7b9244582204f28f5d9b9ea655749fea
lvl0_module2 <Eddiejhunter1000000@gmail.com>
2026-02-07 11:28:07 -0800

    fasdf

---
+++ src/_01_algorithms/_1_prime_or_not/Prime_or_not.java
@@ -0,0 +1,14 @@
+package _01_algorithms._1_prime_or_not;
+
+import javax.swing.JOptionPane;
+
+public class Prime_or_not {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String prime = JOptionPane.showInputDialog("Tell me a number");
+		int primeint = Integer.parseInt(prime);
+		for(int test)
+	}
+
+}

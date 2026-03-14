a5465b8454027ea5271b95072edddda1a25ae6d0
league <maddoxseul@sbsd.net>
2026-03-14 11:31:57 -0700

    asdfasdf

--- src/_02_nested_loops/_3_for_loop_gauntlet/NestedLoops.java
+++ src/_02_nested_loops/_3_for_loop_gauntlet/NestedLoops.java
@@ -4,33 +4,46 @@ public class NestedLoops {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for(int numbers = 0; numbers<101; numbers++) {
-			//System.out.println(numbers);
+		for (int numbers = 0; numbers < 101; numbers++) {
+			// System.out.println(numbers);
 		}
-		for(int loops = 100; loops>=0; loops--) {
-			//System.out.println(loops);
+		for (int loops = 100; loops >= 0; loops--) {
+			// System.out.println(loops);
 		}
-		for(int mouse = 2; mouse<101; mouse+=2) {
-			//System.out.println(mouse);
+		for (int mouse = 2; mouse < 101; mouse += 2) {
+			// System.out.println(mouse);
 		}
-		for(int chair = 1; chair<100; chair+=2) {
-			//System.out.println(chair);
+		for (int chair = 1; chair < 100; chair += 2) {
+			// System.out.println(chair);
 		}
-		for(int eye = 1; eye<501; eye++) {
-			if(eye%2 == 0) {
-				System.out.println(eye +" is even");
+		for (int eye = 1; eye < 501; eye++) {
+			if (eye % 2 == 0) {
+				// System.out.println(eye +" is even");
+			} else {
+				// System.out.println(eye +" is odd");
 			}
-			else {
-				System.out.println(eye +" is odd");
+		}
+		for (int bee = 0; bee < 778; bee += 7) {
+			// System.out.println(bee);
+		}
+
+		for (int jay = 2013; jay < 2027; jay++) {
+			// System.out.println("in " + jay +" i was at some point " + (jay-2013) +" years
+			// old" );
+		}
+
+		for (int nest = 0; nest < 3; nest++) {
+			for (int x = 0; x < 3; x++) {
+				// System.out.println(nest + " " + x);
+
 			}
+
 		}
-		for(int bee = 0; bee<778; bee+=7) {
-			//System.out.println(bee);
-		}
-	
-		for(int jay = 2013; jay<2027; jay++) {
-			System.out.println("in " + jay +" i was at some point " + (jay-2013) +" years old"  );
+		for(int l = 0; l<3; l++){
+			System.out.println();
+			for(int m = 1; m<4; m++) {
+				System.out.print(l*3 + m);
+			}
 		}
 	}
-
 }
